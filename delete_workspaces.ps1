94994481dc75629a26bd7c677c5173ab98b688b3
sowu2 <126790316+sowu2@users.noreply.github.com>
2023-03-06 18:21:31 -0800

    Update delete_workspaces.ps1

--- delete_workspaces.ps1
+++ delete_workspaces.ps1
@@ -1,20 +1,8 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$user_alias,
-    
-    [Parameter(Mandatory=$true)]
-    [string]$resource_group
-)
-
-$workspace1 = "LAWorkspace2-$user_alias"
-$workspace2 = "LAWorkspace3-$user_alias"
-
-Write-Host "Deleting Log Analytics workspaces: $workspace1, $workspace2"
+$workspace1 = "LAWorkspace2-${Env:user_alias}"
+$workspace2 = "LAWorkspace3-${Env:user_alias}"
 
 # Delete workspace1
-Remove-AzOperationalInsightsWorkspace -ResourceGroupName $resource_group -Name $workspace1 -Force
+Remove-AzOperationalInsightsWorkspace -ResourceGroupName ${Env:resource_group} -Name $workspace1 -Force
 
 # Delete workspace2
-Remove-AzOperationalInsightsWorkspace -ResourceGroupName $resource_group -Name $workspace2 -Force
-
-Write-Host "Log Analytics workspaces deleted successfully."
+Remove-AzOperationalInsightsWorkspace -ResourceGroupName ${Env:resource_group} -Name $workspace2 -Force
